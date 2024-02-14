6f5b2c263b0c29a630290cf9b1b1b3f3dafa7201
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2024-02-14 18:31:21 +0100

    add productsCrud for adding action for add, edit and delelte products

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Products;
 use App\Repository\ProductsRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\ProductsType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -34,5 +37,117 @@ class ProductsController extends AbstractController
         return $this->render('pages/products/index.html.twig', [
             'products' => $products
         ]);
+    }
+    /***************************************************************************************** */
+    /**
+     * This controller allow us to create a new product
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    #[Route('/products/nouveau', 'products.new', methods: ['GET', 'POST'])]
+    public function new(
+        Request $request,
+        EntityManagerInterface $manager
+    ): Response {
+        $products = new Products();
+        $form = $this->createForm(ProductsType::class, $products);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $products = $form->getData();
+
+            $manager->persist($products);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre produit a été créé avec succès !'
+            );
+
+            return $this->redirectToRoute('products');
+        }
+
+        return $this->render('pages/products/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+    /******************************************************************************************** */
+
+    /**
+     * This controller allow us to edit products
+     *
+     * @param ProductsRepository $repository
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @param [type] $id
+     * @return Response
+     */
+    #[Route('/products/edition/{id}', 'products.edit', methods: ['GET', 'POST'])]
+    public function edit(
+        ProductsRepository $repository,
+        Request $request,
+        EntityManagerInterface $manager,
+        $id
+    ): Response {
+        $products = $repository->findOneBy(["id" => $id]);
+        $form = $this->createForm(ProductsType::class, $products);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $products = $form->getData();
+
+            $manager->persist($products);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre produit a été modifié avec succès !'
+            );
+
+            return $this->redirectToRoute('products');
+        }
+
+        return $this->render('pages/products/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+    /************************************************************************ */
+    /**
+     * This Controller allow us to delete Products .
+     *
+     * @param EntityManagerInterface $manager
+     * @param ProductsRepository $repository
+     * @param Products $products
+     * @param [type] $id
+     * @return Response
+     */
+
+    #[Route('/products/suppression/{id}', 'products.delete', methods: ['GET'])]
+    public function delete(
+        EntityManagerInterface $manager,
+        ProductsRepository $repository,
+        Products $products,
+        $id
+    ): Response {
+        $products = $repository->findOneBy(["id" => $id]);
+        if (!$products) {
+            $this->addflash(
+                'warning',
+                'Le produit en question n\'a pas été trouvé !'
+            );
+
+            return $this->redirectToRoute('products');
+        }
+        $manager->remove($products);
+        $manager->flush();
+
+        $this->addflash(
+            'success',
+            'Le produits a été supprimé avec succès !'
+        );
+
+        return $this->redirectToRoute('products');
     }
 }
