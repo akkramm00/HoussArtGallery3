c3a2cf49e2bb3a99fa6a56b8213af7c54a07d03c
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2024-02-14 15:56:14 +0100

    add productsCRUD for adding all actions about products

---
+++ src/Controller/ProductsController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductsRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
+
+class ProductsController extends AbstractController
+{
+    /**
+     * This controller dispaly all products
+     *
+     * @param ProductsRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
+    #[Route('/products', name: 'products', methods: ['GET'])]
+    public function index(
+        ProductsRepository $repository,
+        PaginatorInterface $paginator,
+        Request $request
+    ): Response {
+        $products = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1),
+            10
+        );
+
+        return $this->render('pages/products/index.html.twig', [
+            'products' => $products
+        ]);
+    }
+}
