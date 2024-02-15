1669127e6bf0bb8c238c3eaca8400ceeebc3ab88
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2024-02-15 18:25:54 +0100

    add UserSecurity for add users with fixtures, hashing password with EntityListener Methods

--- migrations/Version20240215163825.php
+++ migrations/Version20240215163825.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240214191703 extends AbstractMigration
+final class Version20240215163825 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -23,6 +23,7 @@ final class Version20240214191703 extends AbstractMigration
         $this->addSql('CREATE TABLE colletion (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, artist VARCHAR(255) NOT NULL, category VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, price DOUBLE PRECISION DEFAULT NULL, is_favorite TINYINT(1) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE colletion_products (colletion_id INT NOT NULL, products_id INT NOT NULL, INDEX IDX_F620F08A60F5F4DC (colletion_id), INDEX IDX_F620F08A6C8A81A9 (products_id), PRIMARY KEY(colletion_id, products_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE products (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, price DOUBLE PRECISION NOT NULL, size VARCHAR(255) NOT NULL, property VARCHAR(2000) NOT NULL, artist VARCHAR(255) NOT NULL, category VARCHAR(255) NOT NULL, is_public TINYINT(1) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, full_name VARCHAR(50) NOT NULL, pseudo VARCHAR(50) DEFAULT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE colletion_products ADD CONSTRAINT FK_F620F08A60F5F4DC FOREIGN KEY (colletion_id) REFERENCES colletion (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE colletion_products ADD CONSTRAINT FK_F620F08A6C8A81A9 FOREIGN KEY (products_id) REFERENCES products (id) ON DELETE CASCADE');
@@ -36,6 +37,7 @@ final class Version20240214191703 extends AbstractMigration
         $this->addSql('DROP TABLE colletion');
         $this->addSql('DROP TABLE colletion_products');
         $this->addSql('DROP TABLE products');
+        $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE messenger_messages');
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -9,7 +9,6 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 use Faker\Generator;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
@@ -18,13 +17,11 @@ class AppFixtures extends Fixture
      */
     private Generator $faker;
 
-    private UserPasswordHasherInterface $hasher;
 
-    public function __construct(UserPasswordHasherInterface $hasher)
+    public function __construct()
 
     {
         $this->faker = Factory::create('fr_FR');
-        $this->hasher = $hasher;
     }
 
     public function load(ObjectManager $manager): void
@@ -82,16 +79,16 @@ class AppFixtures extends Fixture
         // $manager->persist($admin);
 
 
-        // for ($i = 0; $i < 10; $i++) {
-        //     $user = new User();
-        //     $user->setFullName($this->faker->name())
-        //         ->setPseudo(mt_rand(0, 1) === 1 ? $this->faker->firstName() : null)
-        //         ->setEmail($this->faker->email())
-        //         ->setRoles(['ROLE_USER'])
-        //         ->setPlainPassword('password');
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setFullName($this->faker->name())
+                ->setPseudo(mt_rand(0, 1) === 1 ? $this->faker->firstName() : null)
+                ->setEmail($this->faker->email())
+                ->setRoles(['ROLE_USER'])
+                ->setPlainPassword('password');
 
-        //     $manager->persist($user);
-        // }
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
