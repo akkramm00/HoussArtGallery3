20b57f8b9abc4e836199a8dcd9d2acadad104c9e
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2024-02-22 17:34:08 +0100

    add all modif for HomeController for adding produsts and collection in the homePage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,12 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProductsRepository;
+use App\Repository\ColletionRepository;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home.index', methods: ['GET'])]
-    public function index(): Response
-    {
-        return $this->render('pages/home/index.html.twig');
+    public function index(
+        ProductsRepository $productsRepository,
+        ColletionRepository $colletionRepository,
+    ): Response {
+        return $this->render('pages/home/index.html.twig', [
+            'products' => $productsRepository->findPublicProducts(3),
+            'colletion' => $colletionRepository->findPublicColletion(3),
+        ]);
     }
 }
