f723ad1c3b3fb7c8e12f7444f6742426af816475
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2024-02-25 00:52:54 +0100

    add aboutController for creating aboutPage ,view and responcive

---
+++ src/Controller/AboutController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AboutController extends AbstractController
+{
+    #[Route('/about', name: 'about.index')]
+    public function index(): Response
+    {
+        return $this->render('pages/about/index.html.twig');
+    }
+}
