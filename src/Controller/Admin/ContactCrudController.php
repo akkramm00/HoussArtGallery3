8846e4a89bae36b9f19f65466231f813b335e870
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2024-03-05 01:41:07 +0100

    add all

---
+++ src/Controller/Admin/ContactCrudController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Contact;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+
+class ContactCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Contact::class;
+    }
+
+    // public function configureCrud(Crud $crud): Crud
+    // {
+    //     return $crud
+    //         ->setEntityLabelInPlural('Demandes de contact')
+    //         ->setEntityLabelInSingular('Demande de contact')
+
+    //         ->setPageTitle("index", "GarageVP - Administration des demandes de contact")
+    //         ->setPaginatorPageSize(10)
+    //         ->addFormTheme('@FOSCKEditor/Form/ckeditor_widget.html.twig');
+    // }
+
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')
+                ->hideOnForm(),
+            TextField::new('fullName'),
+            TextField::new('email')
+                ->setFormTypeOption('disabled', 'disabled'),
+            TextareaField::new('message')
+                ->setFormType(CKEditorType::class),
+            DateTimeField::new('createdAt')
+                ->hideOnForm()
+                ->setFormTypeOption('disabled', 'disabled'),
+
+        ];
+    }
+}
