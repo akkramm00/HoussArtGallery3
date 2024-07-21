66ccb7b85c16066aefd662e66306962c12f19091
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2024-07-21 18:18:41 +0200

    add all

--- migrations/Version20240720153019.php
+++ migrations/Version20240720153019.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240718172513 extends AbstractMigration
+final class Version20240720153019 extends AbstractMigration
 {
     public function getDescription(): string
     {
